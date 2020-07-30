e1a8274a0491d4727c742877298fac2b8b9e4d6e
eeyrw <ayrw@163.com>
2020-07-30 14:37:59 +0800

    M

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+
 extern void hello_from_asm(const unsigned char* str, const unsigned char* str2);
+extern unsigned int ReadCpuBrandStr(unsigned char* str);
 
 int main() {
+    unsigned int a=0;
+    char cpuBstr[16*3];
 	printf("Hello from C\n");
 	hello_from_asm("Hello from ASM1\n", "Hello from ASM2\n");
+	ReadCpuBrandStr(cpuBstr);
+	printf("CPU Brand String: %s\n",cpuBstr);
 	getchar();
 	return 0;
 }
